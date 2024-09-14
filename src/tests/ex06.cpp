f750130fc5e2e1c2aeba53f61f43b02d661a77d6
Plopez230 <plopez230@gmail.com>
2024-09-15 00:52:16 +0200

    test cross product invalid

--- src/tests/ex06.cpp
+++ src/tests/ex06.cpp
@@ -6,7 +6,7 @@
 /*   By: plopez-b <plopez-b@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 20:22:16 by plopez-b          #+#    #+#             */
-/*   Updated: 2024/09/14 20:45:54 by plopez-b         ###   ########.fr       */
+/*   Updated: 2024/09/15 00:51:54 by plopez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 #include <cassert>
 
 void test_cross_product();
+void test_cross_product_invalid();
 
 int main()
 {
     test_cross_product();
+    test_cross_product_invalid();
 
     return 0;
 }
@@ -51,4 +53,25 @@ void test_cross_product()
     assert(exp3 == cross_product(vec5, vec6));
 
     std::cout << "Test cross product passed." << std::endl;
+}
+
+void test_cross_product_invalid()
+{
+    double elements_1[] = {0.0, 0.0, 1.0};
+    double elements_2[] = {1.0, 0.0, 0.0};
+
+
+    Vector<double> vec1(elements_1, 3);
+    Vector<double> vec2(elements_2, 2);
+
+    try
+    {
+        cross_product(vec1, vec2);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "Test cross product invalid passed." << std::endl;
+        return;
+    }
+    throw std::invalid_argument("Test cross product invalid failed.");   
 }
