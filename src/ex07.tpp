7cc51951db95c661769b96250bcddcf786b76571
Plopez230 <plopez230@gmail.com>
2024-09-15 03:04:18 +0200

    ex07

--- src/ex07.tpp
+++ src/ex07.tpp
@@ -6,7 +6,7 @@
 /*   By: plopez-b <plopez-b@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 01:06:29 by plopez-b          #+#    #+#             */
-/*   Updated: 2024/09/15 01:53:52 by plopez-b         ###   ########.fr       */
+/*   Updated: 2024/09/15 03:03:34 by plopez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,39 @@ Vector<K> Matrix<K>::mul_vec(const Vector<K> &v)
     }
 
     Vector<K> result(elements, v_shape[0]);
+
+    return result;
+}
+
+template <typename K>
+Matrix<K> Matrix<K>::mul_mat(const Matrix<K> &mat)
+{
+    const int *this_shape = this->get_shape();
+    const int *m_shape = mat.get_shape();
+
+    if (this_shape[1] != m_shape[0])
+    {
+        throw std::invalid_argument(
+            "Invalid shape.");
+    }
+
+    K elements[this_shape[0] * m_shape[1]];
+    Matrix<K> result(elements, this_shape[0], m_shape[1]);
+
+    for (int m = 0; m < this_shape[0]; m++)
+    {
+        for (int p = 0; p < m_shape[1]; p++)
+        {
+            K element = 0;
+            
+            for (int n = 0; n < m_shape[0]; n++)
+            {
+                element += this->get_element(m, n) * mat.get_element(n, p);
+            }
+            
+            result.set_element(element, m, p);
+        }
+    }
 
     return result;
 }
--- src/tests/ex07.cpp
+++ src/tests/ex07.cpp
@@ -6,7 +6,7 @@
 /*   By: plopez-b <plopez-b@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 01:54:05 by plopez-b          #+#    #+#             */
-/*   Updated: 2024/09/15 02:22:40 by plopez-b         ###   ########.fr       */
+/*   Updated: 2024/09/15 03:01:28 by plopez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 
 void test_mul_vec();
 void test_mul_vec_invalid();
+void test_mul_mat();
+void test_mul_mat_invalid();
 
 int main()
 {
     test_mul_vec();
     test_mul_vec_invalid();
+    test_mul_mat();
+    test_mul_mat_invalid();
 
     return 0;
 }
@@ -71,4 +75,57 @@ void test_mul_vec_invalid()
     }
     
     throw std::invalid_argument("Test mul vec invalid failed.");
+}
+
+void test_mul_mat()
+{
+    double elements_1[] = {1.0, 0.0, 0.0, 1.0};
+    double elements_2[] = {2.0, 1.0, 4.0, 2.0};
+    double elements_3[] = {3.0, -5.0, 6.0, 8.0};
+    double elements_4[] = {1.0, 2.0, -1.0, 0.0, -3.0, -1.0};
+    double elements_5[] = {2.0, 0.0, 1.0, -5.0, 2.0, 3.0};
+
+    double expected_1[] = {1.0, 0.0, 0.0, 1.0};
+    double expected_2[] = {2.0, 1.0, 4.0, 2.0};
+    double expected_3[] = {-14.0, -7.0, 44.0, 22.0};
+    double expected_4[] = {-8.0, 4.0, 7.0, -2.0, 0.0, -1.0, -1.0, -2.0, -6.0};
+
+    Matrix<double> mat_1(elements_1, 2, 2);
+    Matrix<double> mat_2(elements_2, 2, 2);
+    Matrix<double> mat_3(elements_3, 2, 2);
+    Matrix<double> mat_4(elements_4, 3, 2);
+    Matrix<double> mat_5(elements_5, 2, 3);
+
+    Matrix<double> exp_1(expected_1, 2, 2);
+    Matrix<double> exp_2(expected_2, 2, 2);
+    Matrix<double> exp_3(expected_3, 2, 2);
+    Matrix<double> exp_4(expected_4, 3, 3);
+
+    assert(exp_1 == mat_1.mul_mat(mat_1));
+    assert(exp_2 == mat_1.mul_mat(mat_2));
+    assert(exp_3 == mat_3.mul_mat(mat_2));
+    assert(exp_4 == mat_4.mul_mat(mat_5));
+
+    std::cout << "Test mul mat passed." << std::endl;
+}
+
+void test_mul_mat_invalid()
+{
+    double elements_1[] = {1.0, 0.0, 0.0, 1.0};
+    double elements_2[] = {2.0, 1.0, 4.0, 2.0};
+
+    Matrix<double> mat_1(elements_1, 2, 2);
+    Matrix<double> mat_2(elements_2, 4, 1);
+
+    try
+    {
+        mat_1.mul_mat(mat_2);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "Test mul mat invalid passed: " << e.what() << std::endl;
+        return;
+    }
+    
+    throw std::invalid_argument("Test mul mat invalid failed.");
 }
